1c987a69bbd49b2153d8cb0f4b6d440afe2df2de
Hai Gentle <hoanghaik899@gmail.com>
2021-06-23 09:53:32 +0700

    Revert "merge movie"
    This reverts commit 90d090de1dca757b3712b40f81fc8ee4f058eb03, reversing
    changes made to 803c13fa9d8fdb246d011e6f3c80c47d6ed48d76.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       AT CINEMA
-=======
-import './App.css';
-import Container from './container/index'
-
-function App() {
-  return (
-    <div>
-      <Container />
->>>>>>> movie
     </div>
   );
 }
--- src/components/movieContent/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./styles.scss";
-
-export default function MovieContent(props) {
-  const { content } = props;
-  return <div className="cpn-content">{content}</div>;
-}
