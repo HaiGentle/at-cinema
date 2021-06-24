642acff8ca21e94daf0d6bba90f795ea8709fa1b
Hai Gentle <hoanghaik899@gmail.com>
2021-06-24 20:24:29 +0700

    feature UI container header

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,7 +3,7 @@ import './style.scss'
 import classNames from 'classnames';
 
 export default function Input(props) {
-    const { size, suffix, handleOnChange, value } = props;
+    const { size, suffix, ...inputProps } = props;
     const className = classNames(
         "cpn-input",
         size && `cpn-input--${size}`,
@@ -12,8 +12,7 @@ export default function Input(props) {
         <div className="search">
             <input
                 className={className}
-                onChange={handleOnChange}
-                value={value}
+                {...inputProps}
             />
             <div className="search__suffix">
                 {suffix}
---
+++ src/containers/header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './style.scss'
+import logo from '../../assets/image/logo.png'
+import Logo from '../../components/logo'
+import Input from '../../components/input'
+import Button from '../../components/button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+export default function Header(props) {
+    return (
+        <div className="ctn-header">
+            <Logo logo={logo} />
+            <Input
+                suffix={
+                    <Button>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </Button>
+                }
+            />
+        </div>
+    )
+}
