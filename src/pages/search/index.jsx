ea52e00763a2b88d31a02e17e7466821427e7f05
Hai Gentle <hoanghaik899@gmail.com>
2021-07-07 21:27:18 +0700

    add min height

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -11,16 +11,18 @@ const data = [
     {
         title: "Results",
         value:
-            [{ id: "01", alt: "wandavission", name: "WandaVission", img: wandavission, years: 2021 },
-            { id: "02", alt: "ironman", name: "Iron Man", img: wandavission, years: 2021 },
-            { id: "03", alt: "oneday", name: "One Day", img: wandavission, years: 2021 },
-            { id: "04", alt: "cruella", name: "Cruella", img: wandavission, years: 2021 },
-            { id: "05", alt: "mortalkombat", name: "Mortal Kombat", img: wandavission, years: 2021 },
-            { id: "06", alt: "wandavission", name: "WandaVission", img: wandavission, years: 2021 },
-            { id: "07", alt: "ironman", name: "Iron Man", img: wandavission, years: 2021 },
-            { id: "08", alt: "oneday", name: "One Day", img: wandavission, years: 2021 },
-            { id: "09", alt: "cruella", name: "Cruella", img: wandavission, years: 2021 },
-            { id: "10", alt: "mortalkombat", name: "Mortal Kombat", img: wandavission, years: 2021 }]
+            [
+                { id: "01", alt: "wandavission", name: "WandaVission", img: wandavission, years: 2021 },
+                { id: "02", alt: "ironman", name: "Iron Man", img: wandavission, years: 2021 },
+                { id: "03", alt: "oneday", name: "One Day", img: wandavission, years: 2021 },
+                { id: "04", alt: "cruella", name: "Cruella", img: wandavission, years: 2021 },
+                { id: "05", alt: "mortalkombat", name: "Mortal Kombat", img: wandavission, years: 2021 },
+                { id: "06", alt: "wandavission", name: "WandaVission", img: wandavission, years: 2021 },
+                { id: "07", alt: "ironman", name: "Iron Man", img: wandavission, years: 2021 },
+                { id: "08", alt: "oneday", name: "One Day", img: wandavission, years: 2021 },
+                { id: "09", alt: "cruella", name: "Cruella", img: wandavission, years: 2021 },
+                { id: "10", alt: "mortalkombat", name: "Mortal Kombat", img: wandavission, years: 2021 }
+            ]
     },
     // {
     //     title: "Trending TV",
@@ -42,7 +44,9 @@ export default function Search() {
     return (
         <div className="page-search">
             <Header />
-            <Content data={data} />
+            <div className="secsion-content">
+                <Content data={data} />
+            </div>
             <Footer />
         </div>
     )
