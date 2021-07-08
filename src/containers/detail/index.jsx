90fe80a1b13a4c33abb17b395cc22b40f9bb50e9
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-07-08 15:41:21 +0700

    fix pages/detail

--- src/containers/detail/index.jsx
+++ src/containers/detail/index.jsx
@@ -5,44 +5,47 @@ import Title from '../../components/title';
 import MovieName from '../../components/movieName';
 import Rate from '../../components/rate';
 import Button from '../../components/button';
-import wandavission from '../../assets/image/wandavission.jpg'
+import MovieContent from '../../components/movieContent';
 
-export default function ContainerDetail() {
+export default function ContainerDetail(props) {
+    const { data } = props;
     return(
         <div className='ctn-detail'>
-            <div className="ctn-detail-avatar">
-                <MovieImg 
-                    img={wandavission}
-                    size="avatar"
-                />
-            </div>
-            <div className="ctn-detail-review">
-                <MovieName 
-                    name="Wanda Vission"
-                    size="detail"
-                />
-                <div className="ctn-detail-review-name">
-                    <Rate 
-                        rate="7.0"
-                    />
-                    <div className="ctn-detail-review-name-button">
-                        <Button 
-                            mode="play"
-                        >
-                            Play Trailer
-                        </Button>
+            {data.map(item => (
+                <div key={item.id}>
+                    <div className="ctn-detail-avatar">
+                        <MovieImg 
+                            img={item.img}
+                            size="avatar"
+                        />
+                    </div>
+                    <div className="ctn-detail-review">
+                        <MovieName 
+                            name={item.name}
+                            size="detail"
+                        />
+                        <div className="ctn-detail-review-name">
+                            <Rate 
+                                rate={item.rate}
+                            />
+                            <div className="ctn-detail-review-name-button">
+                                <Button 
+                                    mode="play"
+                                    btn={item.btn}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ctn-detail-overview">
+                        <Title
+                            title={item.title}
+                        />
+                        <MovieContent 
+                            content={item.content}
+                        />
                     </div>
                 </div>
-            </div>
-            <div className="ctn-detail-overview">
-                <Title
-                    title="Overview"
-                />
-                <p className="ctn-detail-overview-p">Wonder Woman comes into conflict with the Soviet Union 
-                    during the Cold War in the 1980s and finds a formidable 
-                    foe by the name of the Cheetah.
-                </p>
-            </div>
+            ))}
         </div>
     )
 }
--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import './styles.scss';
 import Header from '../../containers/header';
 import ContainerDetail from '../../containers/detail';
+import Footer from '../../containers/footer'
 import wandavission from '../../assets/image/wandavission.jpg'
+
+const data = [
+    {id: "01", img: wandavission, name: "WandaVission", rate: "7.0", btn: "Play Trailer", title: "Overview", 
+        content: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah"}
+]
+
 
 export default function Detail() {
     return(
         <div className="pages-detail">
             <Header />
-            <img  className="page-detail-cover-img"
+            <img  className="pages-detail-cover-img"
                 src={wandavission}
                 alt='coverimage'
             />
-            <ContainerDetail />
+            <ContainerDetail 
+                data={data}
+            />
+            <Footer />
         </div>
     )
 }
+//fix img
+//footer
