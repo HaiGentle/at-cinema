f1af531719e4fbc26c907b16d16f0c1e6ad134fc
Hai Gentle <hoanghaik899@gmail.com>
2021-07-19 14:28:17 +0700

    done click logo back home page

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './style.scss'
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import logo from '../../assets/image/logo.png'
 import Logo from '../../components/logo'
 import Input from '../../components/input'
 import Button from '../../components/button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
+    let history = useHistory();
+  
+    function handleClickBackHome() {
+        history.push('');
+    }
+    
     return (
         <div className="ctn-header">
-            <Logo logo={logo} />
+            <Logo logo={logo} onClick={handleClickBackHome} />
             <Input
                 suffix={
                     <Button>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,10 @@
 import './styles.scss'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Slides from '../../containers/slides'
 import ListMovieSection from '../../containers/listMovieSection'
 import dataHome from '../../assets/data/api_at_cinema.json'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 export default function Home() {
     const [state, setState] = useState({
